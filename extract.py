a72b23ab6ab3e965d72ab42a3bdf064b559c7c64
razvan <razvan.sandu.lbs@gmail.com>
2021-05-05 12:31:31 +0300

    create date extract2

--- extract.py
+++ extract.py
@@ -1,8 +1,13 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+import json
 
-url =" https://www.primariatm.ro/urbanism/avize-publicitate/"
+url = "https://www.primariatm.ro/wp-json/pmtdata/v1/avize_publicitate"
 page = urlopen(url)
 html = page.read().decode("utf-8")
-soup = BeautifulSoup(html, "html.parser")
-print(soup.get_text())
+data = json.loads(html)
+for elem in data:
+    elem['nr_data']
+    elem['Organizator']
+    elem['Amplasamente']
+    print(f"Nr/Data:{elem['nr_data']} | Org:{elem['Organizator']} | Amplasamente:{elem['Amplasamente']}")
