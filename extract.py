1e83871f531e014bd42b4ea853fe7630c7230595
razvan <razvan.sandu.lbs@gmail.com>
2021-05-05 11:32:59 +0300

    create date extract

--- extract.py
+++ extract.py
@@ -1,1 +1,8 @@
-#code goes here
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+url =" https://www.primariatm.ro/urbanism/avize-publicitate/"
+page = urlopen(url)
+html = page.read().decode("utf-8")
+soup = BeautifulSoup(html, "html.parser")
+print(soup.get_text())
