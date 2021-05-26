06cdb5a25702723d080798621466341aa4e4fee5
andre <spatarua02@gmail.com>
2021-05-26 15:37:10 +0300

    update 3

--- view.py
+++ view.py
@@ -8,16 +8,15 @@ def open_link(link):
     cef.CreateBrowserSync(url=link, window_title="Vizualizare locatii publicitare")
     cef.MessageLoop()
 
-if __name__ == '__main__':
-    with open("output.json") as infile:
-        filecontent = infile.read()
-    data = json.loads(filecontent)
-    html = "<h1>Locatii publicitare</h1>\n"
-    for item in data:
-        html += f"<h3>Locatie publicitara pentru {item['org']}</h3>\n"
-        html += f"<strong>Nr / Data:</strong> {item['nr_data']}<br>\n"
-        html += f"<strong>Amplasament:</strong> {item['amplasament']}<br>\n"
-        html += f"<iframe width='450' height='250' frameborder='0' style='border:0' src='https://www.google.com/maps/embed/v1/place?key=AIzaSyDwY9QtNwODJSd6n4Cm5C0xU3BN-oPq2nE&q={item['locatie']}' allowfullscreen> </iframe> <hr><br>\n"
-    with open("page.html", "w") as file:
-        file.write(html)
-    open_link("file:///page.html")
+with open("output.json") as infile:
+    filecontent = infile.read()
+data = json.loads(filecontent)
+html = "<h1>Locatii publicitare</h1>\n"
+for item in data:
+    html += f"<h3>Locatie publicitara pentru {item['org']}</h3>\n"
+    html += f"<strong>Nr / Data:</strong> {item['nr_data']}<br>\n"
+    html += f"<strong>Amplasament:</strong> {item['amplasament']}<br>\n"
+    html += f"<iframe width='450' height='250' frameborder='0' style='border:0' src='https://www.google.com/maps/embed/v1/place?key=AIzaSyBxFvErb0a3ff7YOfmaKfUCazqCVaktU08&q={item['locatie']}' allowfullscreen> </iframe> <hr><br>\n"
+with open("page.html", "w") as file:
+    file.write(html)
+open_link("file:///page.html")
